2627cea51d266aa6e25d336a00ff28e498672d9b
israel alejandro vazquez escobedo <israelvazquez@MacBook-Pro-de-israel.local>
2018-12-17 05:58:59 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { ICarouselConfig, AnimationConfig } from 'angular4-carousel';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  
 })
 export class AppComponent {
+
+  constructor(private sanitizer: DomSanitizer) {  } 
+
   title = 'angular-prueba';
+  imageUrlArray = ["http://www.aitanatp.com/nivel6/ecosist/images/ecosistemas.jpg",
+  "https://conceptodefinicion.de/wp-content/uploads/2017/03/Interacci%C3%B3n_de_los_Ecosistemas2.jpg",
+"https://sites.google.com/site/losecosistemasdemexico/_/rsrc/1365595137465/introduccion/ecosistemas.jpg"];
+ imagePath = "assets/img/image.jpg";
+
+ urlSinProcesar = "//www.youtube.com/embed/Dv6h-C3MgWg?rel=0";
+
+ urlSaneada = this.sanitizer.bypassSecurityTrustResourceUrl(this.urlSinProcesar);
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {SlideshowModule} from 'ng-simple-slideshow';
+
 
 import { AppComponent } from './app.component';
+import { AccordionComponent } from './accordion/accordion.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { MatToolbarModule,  
+  MatTabsModule,
+  MatButtonModule,
+  MatIconModule,
+  MatCardModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccordionComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    SlideshowModule,
+    NgbModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
